8762847f6a1c4c06d3c8543320f43d8d22838ebc
MinaJahangiri <109921846+MinaJahangiri@users.noreply.github.com>
2022-11-28 10:04:57 -0800

    Add files via upload

---
+++ Mina.Keith.simulation1000.r.r
@@ -0,0 +1,128 @@
+start.simulation<-Sys.time()
+set.seed(1234)
+library(simstudy)
+library(data.table)
+library(msm)
+n.sim<-1000
+n<-1000
+data<-NULL
+for (i in 1:n.sim){
+
+trunc_norm <- function(mean, sd, lower, upper) {
+  rtnorm(n = 1, mean = mean, sd = sd, lower = lower, upper = upper)
+}
+
+defI <- defData(varname = "sex", 
+  formula = 0.5, dist = "binary")
+defI <- defData(defI, varname = "base_age", 
+  formula = "trunc_norm(..mean, ..sd, ..lower, ..upper)",
+  dist = "nonrandom")
+defI <- defData(defI, varname = "theta_bmi", 
+  formula = 0, variance = 4.50^2, dist = "normal")
+defI <- defData(defI, varname = "theta_dbp", 
+  formula = 0, variance = 6.35^2, dist = "normal")
+
+defP <- defDataAdd(varname = "age", formula = "base_age + period*3",
+  dist = "nonrandom")
+defP <- defDataAdd(defP, varname = "bmi", 
+  formula = "19.86 + 0.136*age + 2.360 * sex + theta_bmi",
+  variance = 1.57 ^ 2, dist = "normal")
+defP <- defDataAdd(defP, varname = "dbp", 
+  formula = "55.03 + 0.098 * age - 3.434 * sex + 0.707 * bmi + theta_dbp",
+  variance = 6.88^2, dist = "normal")
+
+mean <- 39.34
+sd <- 16.23
+lower <- 1
+upper <- 84
+
+dd <- genData(n, defI)
+
+dp <- addPeriods(dd, nPeriods = 6)
+dp <- addColumns(defP, dp)
+
+defB <- defCondition(condition = "period == 0", formula = -2.646, dist = "nonrandom")
+defB <- defCondition(defB, "period == 1", formula = -2.634, dist = "nonrandom")
+defB <- defCondition(defB, "period == 2", formula = -3.047, dist = "nonrandom")
+defB <- defCondition(defB, "period == 3", formula = -3.271, dist = "nonrandom")
+defB <- defCondition(defB, "period == 4", formula = -2.872, dist = "nonrandom")
+defB <- defCondition(defB, "period == 5", formula = -2.440, dist = "nonrandom")
+
+defD <- defCondition(condition = "period == 0", formula = -1.701, dist = "nonrandom")
+defD <- defCondition(defD, "period == 1", formula = -1.815, dist = "nonrandom")
+defD <- defCondition(defD, "period == 2", formula = -2.091, dist = "nonrandom")
+defD <- defCondition(defD, "period == 3", formula = -2.386, dist = "nonrandom")
+defD <- defCondition(defD, "period == 4", formula = -2.104, dist = "nonrandom")
+defD <- defCondition(defD, "period == 5", formula = -1.522, dist = "nonrandom")
+
+defM<- defMiss(varname = "bmi", 
+  formula = "int_B + 0.002 * age + 0.02 * dbp", logit.link = TRUE)
+defM<- defMiss(defM, varname = "dbp", 
+  formula = "int_D + 0.002 * age + 0.02 * bmi", logit.link = TRUE)
+
+dp <- addCondition(defB, dp, newvar = "int_B")
+dp <- addCondition(defD, dp, newvar = "int_D")
+
+Miss <- genMiss(dp, defM, 
+  idvars = c("timeID","id", "period", "base_age", "age", "sex", "int_B", "int_D",
+             "theta_bmi", "theta_dbp"))
+
+dobs <- genObs(dp, Miss,
+  idvars = c("timeID","id", "period", "base_age", "age", "sex", "int_B", "int_D",
+             "theta_bmi", "theta_dbp"))
+data<-rbind(dobs,data)
+}
+
+data.sim<-cbind(data,rep(1:n.sim,each=n*6))
+names(data.sim)
+data.sim<- data.sim[,-c(1,4,7:10)]
+names(data.sim)[names(data.sim) == "V2"] <- "dataset"
+nrow(data.sim)
+summary(data.sim)
+write.table(data.sim,"simulation.keith.long.1000.csv",sep=",",col.names=T,row.names=F)
+
+A<-NULL
+for (i in 1:n.sim){
+B<-reshape(data.sim[((i-1)*nrow(data.sim[which(data.sim$dataset==i),])+1):((i-1)*nrow(data.sim[which(data.sim$dataset==i),])+nrow(data.sim[which(data.sim$dataset==i),])),], idvar = "id", timevar = "period", direction = "wide")
+A<-rbind(A,B)
+}
+names(A)
+summary(A)
+nrow(A)
+
+names(A)[names(A) == "age.0"] <- "age0"
+names(A)[names(A) == "age.1"] <- "age1"
+names(A)[names(A) == "age.2"] <- "age2"
+names(A)[names(A) == "age.3"] <- "age3"
+names(A)[names(A) == "age.4"] <- "age4"
+names(A)[names(A) == "age.5"] <- "age5"
+names(A)[names(A) == "bmi.0"] <- "bmi0"
+names(A)[names(A) == "bmi.1"] <- "bmi1"
+names(A)[names(A) == "bmi.2"] <- "bmi2"
+names(A)[names(A) == "bmi.3"] <- "bmi3"
+names(A)[names(A) == "bmi.4"] <- "bmi4"
+names(A)[names(A) == "bmi.5"] <- "bmi5"
+names(A)[names(A) == "dbp.0"] <- "dbp0"
+names(A)[names(A) == "dbp.1"] <- "dbp1"
+names(A)[names(A) == "dbp.2"] <- "dbp2"
+names(A)[names(A) == "dbp.3"] <- "dbp3"
+names(A)[names(A) == "dbp.4"] <- "dbp4"
+names(A)[names(A) == "dbp.5"] <- "dbp5"
+names(A)[names(A) == "sex.0"] <- "sex"
+names(A)[names(A) == "dataset.0"] <- "dataset"
+A<-A[,-c(8,11,13,16,18,21,23,26,28,31)]
+nrow(A)
+names(A)
+write.table(A,"simulation.keith.wide.1000.csv",sep=",",col.names=T,row.names=F)
+
+data1<-A[which(is.na(A$bmi0)==FALSE | is.na(A$bmi1)==FALSE | is.na(A$bmi2)==FALSE | is.na(A$bmi3)==FALSE | is.na(A$bmi4)==FALSE | is.na(A$bmi5)==FALSE),]
+nrow(data1)
+
+data2<-data1[which(is.na(data1$dbp0)==FALSE | is.na(data1$dbp1)==FALSE | is.na(data1$dbp2)==FALSE | is.na(data1$dbp3)==FALSE | is.na(data1$dbp4)==FALSE | is.na(data1$dbp5)==FALSE ),]
+nrow(data2)
+
+write.table(data2,"simulation.keith.1000.csv",sep=",",col.names=T,row.names=F)
+
+end.simulation<-Sys.time()
+time.simulation<- end.simulation-start.simulation
+time.simulation
---
+++ simulation.linearInterpol.locf.r
@@ -0,0 +1,192 @@
+start.simulation<-Sys.time()
+
+set.seed(1234)
+
+##########################
+#Load packages and data
+#########################
+
+library(lme4)
+library(REEMtree)
+library(MuMIn)
+library(merTools)
+library(longitudinalData)
+M<-read.csv("simulation.keith.wide.1000.csv",header=TRUE)
+n.sim<-1000
+
+##########################
+#single imputation
+##########################
+
+#bmi
+copy.mean2.bmi<-NULL
+for (i in 1:n.sim){ #run a loop over the dataframes in the list
+bmi.traj<-as.matrix(M[((i-1)*nrow(M[which(M$dataset==i),])+1):((i-1)*nrow(M[which(M$dataset==i),])+nrow(M[which(M$dataset==i),])),c(4,8,11,14,17,20)])
+imp.bmi<-imputation(bmi.traj,method="linearInterpol.locf",lowerBound="globalMin",upperBound="globalMax")
+copy.mean1.bmi<-as.data.frame(unname(imp.bmi))
+copy.mean2.bmi<-rbind(copy.mean2.bmi,copy.mean1.bmi)
+}
+names(copy.mean2.bmi)
+colnames(copy.mean2.bmi)<-c("bmi0","bmi1","bmi2","bmi3","bmi4","bmi5")
+
+#dbp
+copy.mean2.dbp<-NULL
+for (i in 1:n.sim){ #run a loop over the dataframes in the list
+dbp.traj<-as.matrix(M[((i-1)*nrow(M[which(M$dataset==i),])+1):((i-1)*nrow(M[which(M$dataset==i),])+nrow(M[which(M$dataset==i),])),c(5,9,12,15,18,21)])
+imp.dbp<-imputation(dbp.traj,method="linearInterpol.locf",lowerBound="globalMin",upperBound="globalMax")
+copy.mean1.dbp<-as.data.frame(unname(imp.dbp))
+copy.mean2.dbp<-rbind(copy.mean2.dbp,copy.mean1.dbp)
+}
+names(copy.mean2.dbp)
+colnames(copy.mean2.dbp)<-c("dbp0","dbp1","dbp2","dbp3","dbp4","dbp5")
+###################long data
+data<-cbind(M[,c(1:3,6,7,10,13,16,19)],copy.mean2.bmi,copy.mean2.dbp)
+nrow(data)
+names(data)
+summary(data)
+
+A<-NULL
+for (i in 1:n.sim){
+B<-reshape(data[((i-1)*nrow(data[which(data$dataset==i),])+1):((i-1)*nrow(data[which(data$dataset==i),])+nrow(data[which(data$dataset==i),])),],varying=list(c("age0","age1","age2","age3","age4","age5"),c("bmi0","bmi1","bmi2","bmi3","bmi4","bmi5"),
+c("dbp0","dbp1","dbp2","dbp3","dbp4","dbp5")),idvar="data$id",
+v.names=c("age","bmi","dbp"),times=c(0,1,2,3,4,5),direction="long")
+A<-rbind(B,A)
+}
+names(A)
+nrow(A)
+
+##########################
+#split dataset by imp
+##########################
+
+list_df <- split(A, A$dataset) 
+
+################################################
+#lmer
+###############################################
+###################Coeficient for each imputation of lmer
+result1_df <- as.data.frame(matrix(ncol=5,nrow=length(list_df))) # make an empty dataframe
+colnames(result1_df)<-c("intercept","age","sex","bmi","time") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age +sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+  result1_df[i,]<-fixef(mod) #extract coefficients to dataframe
+}
+result1_df
+mean(result1_df$intercept)
+mean(result1_df$age)
+mean(result1_df$sex)
+mean(result1_df$bmi)
+mean(result1_df$time)
+
+###################StD.Error for each imputation of lmer
+result2_df <- as.data.frame(matrix(ncol=5,nrow=length(list_df))) # make an empty datafram
+colnames(result2_df)<-c("intercept","age","sex","bmi","time") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age + sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result2_df[i,]<- se.fixef(mod) #extract StD.Error to dataframe
+}
+result2_df
+mean(result2_df$intercept)
+mean(result2_df$age)
+mean(result2_df$sex)
+mean(result2_df$bmi)
+mean(result2_df$time)
+
+###################MSE for each imputation of lmer
+result3_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result3_df)<-c("MSE") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age +sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result3_df[i,]<- mean((residuals(mod,type="pearson",scaled=TRUE))^2) #extract MSE to dataframe
+}
+result3_df
+mean(result3_df$MSE)
+
+###################RMSE for each imputation of lmer
+result4_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result4_df)<-c("RMSE") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age + sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result4_df[i,]<- sqrt(mean((residuals(mod,type="pearson",scaled=TRUE))^2)) #extract RMSE to dataframe
+}
+result4_df
+mean(result4_df$RMSE)
+
+###################MAD for each imputation of lmer
+result5_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result5_df)<-c("MAD") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age + sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result5_df[i,]<- mean(abs(residuals(mod,type="pearson",scaled=TRUE))) #extract MAD to dataframe
+}
+result5_df
+mean(result5_df$MAD)
+
+###################AIC for each imputation of lmer
+result6_df <- as.data.frame(matrix(ncol=2,nrow=length(list_df))) # make an empty datafram
+colnames(result6_df)<-c("df","AIC") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age +sex + bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result6_df[i,]<- extractAIC(mod) #extract AIC to dataframe
+}
+matrix(c(result6_df[,2]))
+mean(result6_df[,2])
+
+###################Deviance for each imputation of lmer
+result7_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result7_df)<-c("Deviance") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+  mod<-lmer(dbp~age +sex+ bmi + time+(1|id),data=list_df[[i]]) #mixed model
+    result7_df[i,]<- -2*((summary(mod))$logLik) #extract Deviance to dataframe
+}
+result7_df
+mean(result7_df$Deviance)
+
+################################################
+#REEM trees
+###############################################
+###################MSE for each imputation of REEMtrees
+result8_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result8_df)<-c("MSE") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+mod<-REEMtree(dbp~age +sex+ bmi + time,random=~1|id,data=na.omit(list_df[[i]]),lme.control=lmeControl(opt ="optim"))
+ result8_df[i,]<- mean((residuals(mod))^2) #extract MSE to dataframe
+}
+result8_df
+mean(result8_df$MSE)
+
+###################RMSE for each imputation of REEMtrees
+result9_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result9_df)<-c("RMSE") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+mod<-REEMtree(dbp~age +sex+ bmi + time,random=~1|id,data=na.omit(list_df[[i]]),lme.control=lmeControl(opt ="optim"))
+ result9_df[i,]<- sqrt(mean((residuals(mod))^2)) #extract RMSE to dataframe
+}
+result9_df
+mean(result9_df$RMSE)
+
+###################MAD for each imputation of REEMtrees
+result10_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result10_df)<-c("MAD") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+mod<-REEMtree(dbp~age +sex+ bmi + time,random=~1|id,data=na.omit(list_df[[i]]),lme.control=lmeControl(opt ="optim"))
+    result10_df[i,]<- mean(abs(residuals(mod))) #extract MAD to dataframe
+}
+result10_df
+mean(result10_df$MAD)
+
+###################Deviance for each imputation of REEMtrees
+result11_df <- as.data.frame(matrix(ncol=1,nrow=length(list_df))) # make an empty datafram
+colnames(result11_df)<-c("Deviance") #give the dataframe column names
+for (i in 1:length(list_df)){ #run a loop over the dataframes in the list
+mod<-REEMtree(dbp~age +sex+ bmi + time,random=~1|id,data=na.omit(list_df[[i]]),lme.control=lmeControl(opt ="optim"))
+    result11_df[i,]<- -2*(logLik.REEMtree(mod)) #extract Deviance to dataframe
+}
+result11_df
+mean(result11_df$Deviance)
+
+end.simulation<-Sys.time()
+time.simulation<- end.simulation-start.simulation
+time.simulation
+
+
